b9884604dff848f0d09106e551f2e6567b52cca1
Thanf-code <82493550+Thanf-code@users.noreply.github.com>
2021-08-26 17:40:15 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,4 +21,4 @@
 
 #sed -i '$a src-git helloworld https://github.com/P3TERX/helloworld' feeds.conf.default
 
-sed -i '$a src-git package https://github.com/Thanf-code/op-packages' feeds.conf.default
+#sed -i '$a src-git package https://github.com/Thanf-code/op-packages' feeds.conf.default
