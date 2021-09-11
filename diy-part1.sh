812db7968cb89ec6c7a7b87a27eec0adcfd181b8
Thanf-code <82493550+Thanf-code@users.noreply.github.com>
2021-09-11 11:45:43 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,7 +17,7 @@
 
 #sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 
-#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default          # 科学上网基本依赖（编译bypass、openclash需要，passwall不需要）
 
 #sed -i '$a src-git helloworld https://github.com/P3TERX/helloworld' feeds.conf.default
 
