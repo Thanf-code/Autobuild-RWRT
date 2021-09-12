3d7c3d2115b138cc91beb9a4c60df0fcb86bc5b7
Thanf-code <82493550+Thanf-code@users.noreply.github.com>
2021-09-12 12:46:51 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,6 +23,13 @@ sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
  # 设置密码为空
 sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ                                                            
 
+# 修改bypass支持lean源码shadowsocksr-libev-ssr-redir/server重命名
+find package/*/ feeds/*/ -maxdepth 5 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
+find package/*/ feeds/*/ -maxdepth 5 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
+
+# 修改bypass依赖为smartdns支持
+find package/*/ feeds/*/ -maxdepth 5 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/smartdns-le/smartdns/g' {}
+
 # Add kernel build user
 [ -z $(grep "CONFIG_KERNEL_BUILD_USER=" .config) ] &&
     echo 'CONFIG_KERNEL_BUILD_USER="Thanf"' >>.config ||
