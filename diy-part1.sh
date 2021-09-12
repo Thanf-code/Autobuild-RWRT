2604ab23a2d0b383c0223f264a7d8cb9e37b1f23
Thanf-code <82493550+Thanf-code@users.noreply.github.com>
2021-09-12 12:13:49 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,7 @@
 #sed -i 's@coolsnowwolf/packages@P3TERX/packages@' feeds.conf.default
 #sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 #svn co https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall > /dev/null 2>&1
-#sed -i '$a src-git small https://github.com/Thanf-code/small' feeds.conf.default          # 科学上网基本依赖（编译bypass、openclash需要，passwall不需要）
+sed -i '$a src-git small https://github.com/Thanf-code/small' feeds.conf.default          # 科学上网基本依赖（编译bypass、openclash需要，passwall不需要）
 #sed -i '$a src-git helloworld https://github.com/P3TERX/helloworld' feeds.conf.default
 #svn co https://github.com/Thanf-code/luci-app-bypass package/luci-app-bypass > /dev/null 2>&1
 svn co https://github.com/garypang13/openwrt-bypass package/luci-app-bypass > /dev/null 2>&1
