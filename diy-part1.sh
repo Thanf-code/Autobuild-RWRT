0e09a460ac05ae7d07a5518d2186b0bacab80e9c
Thanf-code <82493550+Thanf-code@users.noreply.github.com>
2021-09-25 10:48:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,5 +19,5 @@ svn co https://github.com/Thanf-code/iptvhelper package/iptvhelper > /dev/null 2
 #git clone https://github.com/Hyy2001X/luci-app-autoupdate.git package/luci-app-autoupdate
 #git clone https://github.com/Thanf-code/op-package/tree/main/luci-app-control-webrestriction.git package/luci-app-control-webrestriction
 git clone https://github.com/Thanf-code/luci-app-control-weburl.git package/luci-app-control-weburl
-git clone -b 19.07 https://github.com/Thanf-code/luci-app-omcproxy.git package/luci-app-omcproxy
+git clone -b https://github.com/Thanf-code/luci-app-omcproxy.git package/luci-app-omcproxy
 git clone -b 19.07 https://github.com/Thanf-code/small-package.git
