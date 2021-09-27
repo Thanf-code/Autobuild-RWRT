7fc8b1dbc610b2109fa99a0509301bf3ff6c7b8f
Thanf-code <82493550+Thanf-code@users.noreply.github.com>
2021-09-27 21:29:57 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,7 +13,7 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #sed -i 's@coolsnowwolf/packages@P3TERX/packages@' feeds.conf.default
 #sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
-sed -i '$a src-git small https://github.com/Thanf-code/small-package' feeds.conf.default
+#sed -i '$a src-git small https://github.com/Thanf-code/small-package' feeds.conf.default
 svn co https://github.com/Thanf-code/OpenAppFilter package/OpenAppFilter > /dev/null 2>&1
 svn co https://github.com/Thanf-code/iptvhelper package/iptvhelper > /dev/null 2>&1
 #git clone https://github.com/Hyy2001X/luci-app-autoupdate.git package/luci-app-autoupdate
