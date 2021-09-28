009a88c1053a5ce1038ea2c36dab99020f140b6b
Thanf-code <82493550+Thanf-code@users.noreply.github.com>
2021-09-28 19:12:33 +0800

    Update and rename common.sh to official.sh

--- diy-part1/official.sh
+++ diy-part1/official.sh
@@ -19,5 +19,5 @@ sed -i '$a src-git small https://github.com/Thanf-code/small-package' feeds.conf
 #git clone https://github.com/Hyy2001X/luci-app-autoupdate.git package/luci-app-autoupdate
 #git clone https://github.com/Thanf-code/op-package/tree/main/luci-app-control-webrestriction.git package/luci-app-control-webrestriction
 git clone https://github.com/Thanf-code/luci-app-control-weburl.git package/luci-app-control-weburl
-git clone https://github.com/Thanf-code/luci-app-omcproxy.git package/luci-app-omcproxy
+#git clone https://github.com/Thanf-code/luci-app-omcproxy.git package/luci-app-omcproxy
 #git clone -b 19.07 https://github.com/Thanf-code/small-package.git package/small
