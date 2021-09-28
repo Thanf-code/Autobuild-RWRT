e08d9013a15cc4039dc7388a28273a7504f827df
Thanf-code <82493550+Thanf-code@users.noreply.github.com>
2021-09-28 19:10:52 +0800

    Create lean.sh

---
+++ diy-part1/lean.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#=============================================================
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=============================================================
+
+# fw876/helloworld
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.defaultault
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#sed -i 's@coolsnowwolf/packages@P3TERX/packages@' feeds.conf.default
+#sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+sed -i '$a src-git small https://github.com/Thanf-code/small-package' feeds.conf.default
+svn co https://github.com/Thanf-code/OpenAppFilter package/OpenAppFilter > /dev/null 2>&1
+svn co https://github.com/Thanf-code/iptvhelper package/iptvhelper > /dev/null 2>&1
+#git clone https://github.com/Hyy2001X/luci-app-autoupdate.git package/luci-app-autoupdate
+#git clone https://github.com/Thanf-code/op-package/tree/main/luci-app-control-webrestriction.git package/luci-app-control-webrestriction
+git clone https://github.com/Thanf-code/luci-app-control-weburl.git package/luci-app-control-weburl
+git clone -b 18.06 https://github.com/Thanf-code/luci-app-omcproxy.git package/luci-app-omcproxy
