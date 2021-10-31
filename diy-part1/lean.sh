4a7630e4a41eb7bbfb255c8419fdee2e6e9bfc10
Thanf-code <82493550+Thanf-code@users.noreply.github.com>
2021-10-31 15:42:24 +0800

    Update lean.sh

--- diy-part1/lean.sh
+++ diy-part1/lean.sh
@@ -17,6 +17,5 @@ sed -i '$a src-git small https://github.com/Thanf-code/small-package' feeds.conf
 svn co https://github.com/Thanf-code/OpenAppFilter package/OpenAppFilter > /dev/null 2>&1
 svn co https://github.com/Thanf-code/iptvhelper package/iptvhelper > /dev/null 2>&1
 #git clone https://github.com/Hyy2001X/luci-app-autoupdate.git package/luci-app-autoupdate
-#git clone https://github.com/Thanf-code/op-package/tree/main/luci-app-control-webrestriction.git package/luci-app-control-webrestriction
-git clone https://github.com/Thanf-code/luci-app-control-weburl.git package/luci-app-control-weburl
+
 git clone -b 18.06 https://github.com/Thanf-code/luci-app-omcproxy.git package/luci-app-omcproxy
