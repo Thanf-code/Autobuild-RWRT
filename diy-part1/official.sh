da413cfe5c8a086406497aa269ca7308c5c7cecf
Thanf-code <82493550+Thanf-code@users.noreply.github.com>
2021-11-06 09:52:08 +0800

    Update official.sh

--- diy-part1/official.sh
+++ diy-part1/official.sh
@@ -16,5 +16,5 @@
 sed -i '$a src-git small https://github.com/Thanf-code/small-package' feeds.conf.default
 svn co https://github.com/Thanf-code/iptvhelper package/iptvhelper > /dev/null 2>&1
 #git clone https://github.com/Hyy2001X/luci-app-autoupdate.git package/luci-app-autoupdate
-git clone https://github.com/Thanf-code/luci-app-control-weburl.git package/luci-app-control-weburl
+#git clone https://github.com/Thanf-code/luci-app-control-weburl.git package/luci-app-control-weburl
 #git clone https://github.com/mwarning/zerotier-openwrt.git package/zerotier
