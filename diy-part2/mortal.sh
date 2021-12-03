d99fb62affc8ca5ff9dabb54fa32119338e9d66a
Thanf-code <82493550+Thanf-code@users.noreply.github.com>
2021-12-03 12:03:52 +0800

    Update mortal.sh

--- diy-part2/mortal.sh
+++ diy-part2/mortal.sh
@@ -26,6 +26,9 @@ sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/emortal/default-settings/files/
 # 换源
 sed -i 's,downloads.openwrt.org,mirrors.tencent.com/lede,g' /etc/opkg/distfeeds.conf
 
+# ramips机型,默认内核5.4，修改内核为5.10
+#sed -i 's/PATCHVER:=5.4/PATCHVER:=5.10/g' target/linux/ramips/Makefile
+
 # Add kernel build user
 [ -z $(grep "CONFIG_KERNEL_BUILD_USER=" .config) ] &&
     echo 'CONFIG_KERNEL_BUILD_USER="Thanf"' >>.config ||
